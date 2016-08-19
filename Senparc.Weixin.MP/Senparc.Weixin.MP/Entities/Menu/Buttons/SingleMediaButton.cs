be9a81c0c74a75517c3e8f25428618fb69d57b7e
hugejile <hugejile@msn.com>
2016-08-19 09:33:55 +0800

    add two buttons

---
+++ Senparc.Weixin.MP/Senparc.Weixin.MP/Entities/Menu/Buttons/SingleMediaButton.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Senparc.Weixin.MP.Entities.Menu.Buttons
+{
+    /// <summary>
+    /// 用户点击media_id类型按钮后，微信服务器会将开发者填写的永久素材id对应的素材下发给用户，
+    /// 永久素材类型可以是图片、音频、视频、图文消息。
+    /// 请注意：永久素材id必须是在“素材管理/新增永久素材”接口上传后获得的合法id。
+    /// </summary>
+    public class SingleMediaButton : SingleButton
+    {
+        public SingleMediaButton()
+            : base(ButtonType.media_id.ToString())
+        {
+
+        }
+        public string name { get; set; }
+
+        public string media_id { get; set; }
+    }
+}
